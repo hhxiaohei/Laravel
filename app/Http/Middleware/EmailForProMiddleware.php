95b6bd44a52889bef85cdfda77715b6480fc0cdd
MikeZhang199 <hhxiaohei1@163.com>
2016-11-01 17:18:46 +0800

    Update EmailForProMiddleware.php

--- app/Http/Middleware/EmailForProMiddleware.php
+++ app/Http/Middleware/EmailForProMiddleware.php
@@ -19,14 +19,14 @@ class EmailForProMiddleware
         $rs = $next($request);
         if($request->user()){
             $mail = new Message;
-            $mail->setFrom('点点贷官方网站 <hhzhanghongyi@163.com>')
+            $mail->setFrom('点点贷官方网站 <@163.com>')
                 ->addTo($request->user()->email)
                 ->setSubject('点点贷理财——金九银十，理财正当时！@官网')
                 ->setBody("<h1>点点贷理财——金九银十，理财正当时！@官网</h1><br>用户名:".$request->user()->name.',共购买产品'.$request->v_amount.'元,'.'产品单号:'.$request->v_oid);
             $mailer = new SmtpMailer([
                 'host' => 'smtp.163.com',
-                'username' => 'hhzhanghongyi',
-                'password' => 'qq350000225621'
+                'username' => '',
+                'password' => ''
             ]);
             $mailer->send($mail);
         }
